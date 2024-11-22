5f90960be7b1fa42ae404ce6a5ae2766e5a57753
Sarah Burke Spolaor <sarahbspolaor@gmail.com>
2024-11-22 12:01:45 -0500

    initiated db communications restructure. db_info read is now built into the communications __init__.py

--- bobcat_db_interface/communications/__init__.py
+++ bobcat_db_interface/communications/__init__.py
@@ -1,1 +1,8 @@
+import os
 from . import db_comms
+
+
+# Read in personal keys and info from user-based file.
+# This info file lives in ~/.bobcat/db_info.txt
+db_info_file = "{0}/{1}".format(os.getenv("HOME"),".bobcat/db_info.txt")
+db_info = {line.split(":", 1)[0].strip(): line.split(":", 1)[1].strip() for line in open(db_info_file) if ':' in line}

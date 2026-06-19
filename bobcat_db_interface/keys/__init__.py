4e12cdbceb68f9153f1f7853ecf81b6ee7a7ba48
Sarah Burke Spolaor <sarahbspolaor@gmail.com>
2026-06-18 23:45:39 -0400

    I modified this to be able to run on Dominic's Docker. To return to our previous mode you should change your .bobcat/db_info.txt and your __init__.py file in keys/

--- bobcat_db_interface/keys/__init__.py
+++ bobcat_db_interface/keys/__init__.py
@@ -1,4 +1,13 @@
 import os
 
 db_info_file = "{0}/{1}".format(os.getenv("HOME"),".bobcat/db_info.txt")
-db_info = {line.split(":", 1)[0].strip(): line.split(":", 1)[1].strip() for line in open(db_info_file) if ':' in line}
+
+# This maps to Dominic's docker set-up
+db_info = {
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "pass": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST", "db"),
+    "port": os.getenv("POSTGRES_PORT", "5432"),
+}
+#db_info = {line.split(":", 1)[0].strip(): line.split(":", 1)[1].strip() for line in open(db_info_file) if ':' in line}
